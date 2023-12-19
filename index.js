48d28cd1094f6dd5ccf74f9b0afcde86bd75e513
Igor Teles Caetano <igor.caetano@grupobamaq.com.br>
2023-12-19 15:55:43 -0300

    Como utilizar expressões regulares, uma sintaxe feita para reconhecer padrões de texto, para capturar links e URLs; Como utilizamos classes e grupos de expressões regulares para refinar a busca por estes padrões, além dos métodos que o JavaScript tem (por exemplo match e exec) para trazer os resultados e trabalhar com eles; Manipular os resultados extraídos do texto através de expressões regulares e montar um objeto de retorno utilizando a sintaxe do JavaScript e desestruturação de objetos com o spread operator ....

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
+
+function extraiLinks(texto) {
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm; //tenho que colar o regex entre barras invertidas
+    const capturas = [...texto.matchAll(regex)]; //matchAll retorna um objeto iterável, por isso o spread operator
+    const resultados = capturas.map((captura) => ({[captura[1]] : captura[2]})) 
+    return resultados;
+}
+
 function trataErro (erro) {
     console.log(erro);
     throw new Error(chalk.red(erro.code, 'Não há arquivo no diretório'));
@@ -12,21 +20,10 @@ async function pegaArquivo(caminhoDoArquivo) {
     try {
     const encoding = 'utf-8';
     const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
-    console.log(chalk.green(texto));
+    console.log(extraiLinks(texto));
     } catch (erro) {
         trataErro(erro);
     }
 }
 
-// //promises com then
-
-// function pegaArquivo(caminhoDoArquivo) {
-//     const encoding = 'utf-8';
-//    fs.promises
-//     .readFile(caminhoDoArquivo, encoding)
-//     .then((texto) => console.log(chalk.green(texto)))
-//     .catch(trataErro);
-// }
-
 pegaArquivo('./arquivos/texto.md');
-pegaArquivo('./arquivos/');
